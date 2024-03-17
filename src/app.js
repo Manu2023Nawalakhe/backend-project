672c5f176015c2216061b5b15958c69b534652ac
Manu2023Nawalakhe <manishanawalakhe@gmail.com>
2024-03-17 11:30:31 +0530

    all configration complet

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: process.env.CORS.ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
@@ -13,5 +13,15 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:8000/api/v1/users/register
 
 export { app };
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  await res.send(200).json({
+    message: "ok",
+  });
+});
+export { registerUser };
