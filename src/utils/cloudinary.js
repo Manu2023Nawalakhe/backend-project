41648a68bde31bc168dfa8c2219204f4604c02a6
Manu2023Nawalakhe <manishanawalakhe@gmail.com>
2024-03-18 14:56:12 +0530

    fix all register controller bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,9 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     //file has been uploaded successfull
 
-    console.log("file is uploaded on cloudinary", response.url);
+    //console.log("file is uploaded on cloudinary", response.url);
+
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
