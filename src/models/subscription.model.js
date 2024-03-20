16cc1095978d149645295356fc15273766801000
Manu2023Nawalakhe <manishanawalakhe@gmail.com>
2024-03-20 21:47:07 +0530

    Add user Account management

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscribe: {
+      type: Schema.Types.ObjectId, //one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, //one to whom 'subscriber' is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
